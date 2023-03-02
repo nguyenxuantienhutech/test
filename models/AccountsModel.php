fc3bd6647deef7f6b8c0cc8550b1d2290b787993
TIENNX <xuantien8a1@gmail.com>
2023-03-02 10:33:19 +0700

    new update

--- models/AccountsModel.php
+++ models/AccountsModel.php
@@ -10,6 +10,7 @@ class AccountsModel
     }
 
     // CRUD 
+    //add
     public function addAccount($fullName, $email, $password, $role)
     {
         $sql = "INSERT INTO accounts (name, email, password, account_type) VALUES ('$fullName', '$email', '$password', '$role')";
@@ -18,6 +19,18 @@ class AccountsModel
         } else {
             return false;
         }
+    }
+    //delete
+    public function deleteAccount($id){
+        $sql = "DELETE FROM accounts WHERE id = '$id'";
+        $user = mysqli_query($this->conn, $sql);
+        return $user;
+    }
+    //update
+    public function updateAccount($id,$fullName, $email, $password){
+        $sql = "UPDATE accounts SET name = 'Nguyen Van B', email ='nguyenvanaa@gmail.com', password`='123123123A', account_type = 'admin' WHERE id = $id";
+        $user = mysqli_query($this->conn, $sql);
+        return $user;
     }
     // thêm các phương thức khác để thao tác với bảng accounts
     public function getAccounts($email, $password)
